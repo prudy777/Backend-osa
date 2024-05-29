2c6a53b4e84e5817cf0962dc7325ff376488f1f2
Daniel Ailemen <daniel.ailemen07@gmail.com>
2024-05-29 21:58:07 +0100

    ready

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
